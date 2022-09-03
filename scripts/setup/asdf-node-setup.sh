ea6fde94f8b6da8cea99151408996433eeae5075
leandrorezendecoutinho <eleandro.rc.contato@gmail.com>
2022-09-03 18:51:34 -0300

    update node to lts version and install yarn by default

--- scripts/setup/asdf-node-setup.sh
+++ scripts/setup/asdf-node-setup.sh
@@ -2,6 +2,8 @@ echo "Starting nodejs setup"
 
 asdf plugin add nodejs
 
-asdf install nodejs 18.7.0
+asdf install nodejs 16.17.0
 
-asdf global nodejs 18.7.0
+asdf global nodejs 16.17.0
+
+npm install --global yarn
